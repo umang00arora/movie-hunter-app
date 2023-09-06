b09e0ee9c04b2760bf5cf98b2b3d6b5d825df41c
umang00arora <umangarora810@gmail.com>
2023-09-06 17:43:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {HashRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
@@ -15,7 +15,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
     <GlobalTheme>
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route element={<MainLayout/>}>
       <Route path="/" element={<Home />}/>
@@ -24,7 +24,7 @@ function App() {
       <Route path='/show/:showId' element={<Show/>}/>
       <Route path='*' element={<div>Not Found</div>}/>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
   </GlobalTheme>
   </QueryClientProvider>
   );
